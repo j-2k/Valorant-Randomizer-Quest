c3bccfb909802401d74604eb3d13ffc7c8ba467f
Juma <juma_ad@hotmail.com>
2024-07-03 18:07:42 +0400

    Layout before dev done

--- ValorantQuestByJuma/src/LoadoutForm.Designer.cs
+++ ValorantQuestByJuma/src/LoadoutForm.Designer.cs
@@ -80,6 +80,7 @@
             this.textBox1.Size = new System.Drawing.Size(100, 26);
             this.textBox1.TabIndex = 22;
             this.textBox1.Text = "9000";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // moneyTitle
